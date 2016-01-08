322f1c6874803a6b26246db4132fc470eaf5ffb2
Pan-Masa <nasmac@NasMacs-MacBook-Air.local>
2016-01-09 02:26:26 +0700

    added cocoapods/

---
+++ cocoapods/cocoapods/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  cocoapods
+//
+//  Created by Nas@Mac on 3/1/59.
+//  Copyright © พ.ศ. 2559 Nas@Mac. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
